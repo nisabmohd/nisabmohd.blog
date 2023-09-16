891cdf34aef4284d74f23f5a1f672ac3062d718e
Mohd. Nisab <nisabmohd@gmail.com>
2023-09-17 00:55:44 +0530

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -28,7 +28,7 @@ export default async function page({
   if (!result) return NotFound();
 
   return (
-    <div className="m-auto prose prose-sm dark:prose-invert prose-neutral prose-code:dark:bg-neutral-950 prose-pre:dark:bg-neutral-950   prose-code:bg-neutral-900 prose-pre:bg-neutral-900 prose-pre:rounded-md prose-headings:dark:text-neutral-50 prose-p:dark:text-neutral-100 prose-li:dark:text-neutral-100 prose-ol:dark:text-neutral-100 prose-strong:dark:text-neutral-100 prose-a:dark:text-neutral-100 max-w-none prose-img:m-auto mt-5 pb-6">
+    <div className="m-auto prose prose dark:prose-invert prose-neutral prose-code:dark:bg-neutral-950 prose-pre:dark:bg-neutral-950   prose-code:bg-neutral-900 prose-pre:bg-neutral-900 prose-pre:rounded-md prose-headings:dark:text-neutral-50 prose-p:dark:text-neutral-100 prose-li:dark:text-neutral-100 prose-ol:dark:text-neutral-100 prose-strong:dark:text-neutral-100 prose-a:dark:text-neutral-100 max-w-none prose-img:m-auto mt-5 pb-6">
       <h1>{result.frontmatter.title}</h1>
       {result.content}
     </div>
