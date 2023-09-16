8f3c0cfe11036419fb2d601e142b5a57f7fbc462
nisabmohd <nisabmohd@gmail.com>
2023-09-17 00:39:12 +0530

    loading skeleton responsiveness

--- src/app/blog/[slug]/loading.tsx
+++ src/app/blog/[slug]/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col gap-2 w-[32%] max-[1200px]:w-[85%] max-[500px]:w-[91%] m-auto h-screen py-5 ">
+    <div className="flex flex-col gap-2">
       <Skeleton className="w-[80%] h-[40px] rounded-md mb-8" />
       <Skeleton className="w-full h-[20px] rounded-md" />
       <Skeleton className="w-full h-[20px] rounded-md" />
