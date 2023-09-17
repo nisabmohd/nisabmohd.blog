9bb2e9a31804d77c50f242613b2c91f376002a5c
nisabmohd <nisabmohd@gmail.com>
2023-09-17 19:37:43 +0530

    some footer changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className=" w-[95%] m-auto text-sm dark:text-slate-300 py-4 mt-2 text-slate-800 flex flex-row justify-center items-center gap-2 text-center">
-      <Command className="w-4 h-4" />
+      <Command className="w-4 h-4 max-[500px]:hidden" />
       <span>
         Built by{" "}
         <Link
