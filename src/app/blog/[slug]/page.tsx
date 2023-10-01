6a3d2dc56162175b8635d6b4bc55e011ddeaf0eb
nisabmohd <nisabmohd@gmail.com>
2023-10-01 12:58:39 +0530

    markdowns changes and addition

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -12,7 +12,7 @@ export default async function page({
 
   return (
     <div className="flex flex-col items-center">
-      <div className="prose prose-md font-mono dark:prose-invert prose-slate prose-code:text-[15px] prose-code:bg-zinc-900 max-[500px]:max-w-xs max-w-sm sm:max-w-prose">
+      <div className="prose prose-md font-mono dark:prose-invert prose-slate prose-code:text-[14px] prose-code:bg-zinc-900 max-[500px]:max-w-xs max-w-sm sm:max-w-prose">
         <h1>{md.current.frontmatter.title}</h1>
         {md.current.content}
         <MarkdownSlider previous={md.previous} next={md.next} />
