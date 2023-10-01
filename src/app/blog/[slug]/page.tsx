b741e8ff5699987ff8d55731cf1b453bc85c77cb
nisabmohd <nisabmohd@gmail.com>
2023-10-01 19:31:27 +0530

    light mode syntax highliting

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -12,7 +12,7 @@ export default async function page({
 
   return (
     <div className="flex flex-col items-center">
-      <div className="prose prose-md font-mono dark:prose-invert prose-slate prose-code:text-[14px] prose-code:bg-zinc-900 prose-pre:bg-zinc-900 max-[500px]:max-w-xs max-w-sm sm:max-w-prose">
+      <div className="prose prose-md font-mono dark:prose-invert prose-slate prose-code:text-[14px] dark:prose-code:text-zinc-200 prose-code:text-zinc-800 dark:prose-code:bg-zinc-900 dark:prose-pre:bg-zinc-900 prose-code:bg-zinc-50 prose-pre:bg-zinc-50 max-[500px]:max-w-xs max-w-sm sm:max-w-prose">
         <h1>{md.current.frontmatter.title}</h1>
         {md.current.content}
         <MarkdownSlider previous={md.previous} next={md.next} />
--- src/components/md-prev-next.tsx
+++ src/components/md-prev-next.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
 import { ChevronRight, ChevronLeft } from "lucide-react";
 import { MDXFrontmatter } from "@/lib/md";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function MarkdownSlider({
   next,
@@ -10,28 +16,49 @@ export default function MarkdownSlider({
   next: MDXFrontmatter;
 }) {
   return (
-    <div className="pagination flex flex-row items-center justify-between w-full pt-5">
-      <div className="max-w-[50%]">
-        {previous && (
-          <Link
-            href={`/blog/${previous?.slug}`}
-            className="flex flex-row items-center gap-2 no-underline"
-          >
-            <ChevronLeft className="w-4 h-4" />
-            {previous?.title}
-          </Link>
-        )}
+    <TooltipProvider>
+      <div className="pagination flex flex-col sm:flex-row items-center justify-between w-full pt-5">
+        <div className="max-w-[50%]">
+          {previous && (
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  href={`/blog/${previous?.slug}`}
+                  className="flex flex-row items-center justify-center gap-2 no-underline"
+                >
+                  <ChevronLeft className="w-4 h-4" />
+                  {previous.title.length
+                    ? previous.title.slice(0, 30) + "..."
+                    : previous.title}
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-xs font-light">{previous.title}</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
+        </div>
+        <div className="max-w-[50%]">
+          {next && (
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  className="flex flex-row items-center gap-2 no-underline"
+                  href={`/blog/${next?.slug}`}
+                >
+                  {next.title.length > 40
+                    ? next.title.slice(0, 30) + "..."
+                    : next.title}{" "}
+                  <ChevronRight className="w-4 h-4" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-xs font-light">{next.title}</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
+        </div>
       </div>
-      <div className="max-w-[50%]">
-        {next && (
-          <Link
-            className="flex flex-row items-center gap-2 no-underline"
-            href={`/blog/${next?.slug}`}
-          >
-            {next?.title} <ChevronRight className="w-4 h-4" />
-          </Link>
-        )}
-      </div>
-    </div>
+    </TooltipProvider>
   );
 }
