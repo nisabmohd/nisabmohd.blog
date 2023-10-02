623310772e53ad059ccae2176064a4a8b3c117fa
nisabmohd <nisabmohd@gmail.com>
2023-10-02 15:25:53 +0530

    small devices skeleton responsiveness

--- src/app/blog/[slug]/loading.tsx
+++ src/app/blog/[slug]/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="">
       <div className="prose prose-md font-mono dark:prose-invert prose-slate max-[500px]:max-w-[100%] max-w-sm sm:max-w-prose mx-auto">
-        <Skeleton className="w-[80%] h-[40px] rounded-sm" />
+        <Skeleton className="w-[80%] h-[38px] rounded-sm" />
         <Skeleton className="w-[40%] h-[20px] rounded-sm mt-2 " />
         <div className="border-b-2 mb-8 pb-8"></div>
         <Skeleton className="w-[100%] h-[20px] rounded-sm mt-2 " />
--- src/components/blog-card-skeleton.tsx
+++ src/components/blog-card-skeleton.tsx
@@ -3,15 +3,17 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function BlogSkeletonCard() {
   return (
     <div className="flex flex-col gap-1">
-      <Skeleton className="w-[16%] h-[20px] rounded-sm" />
-      <Skeleton className="w-[85%] h-[40px] rounded-sm  mt-2" />
+      <Skeleton className="w-[160px] h-[20px] rounded-sm" />
+      <Skeleton className="sm:w-[85%] w-[98%] h-[38px] rounded-sm  mt-2" />
+      <Skeleton className="sm:hidden w-[68%] h-[38px] rounded-sm  mt-2" />
+
       <div className="flex flex-row items-center gap-4 my-2">
         <Skeleton className="w-[60px] h-[20px] rounded-sm" />
         <Skeleton className="w-[55px] h-[20px] rounded-sm" />
         <Skeleton className="w-[55px] h-[20px] rounded-sm" />
       </div>
       <Skeleton className="w-[98%] h-[20px] rounded-sm" />
-      <Skeleton className="w-[65%] h-[20px] rounded-sm mt-1" />
+      <Skeleton className="sm:w-[65%] w-[85%] h-[20px] rounded-sm mt-1" />
     </div>
   );
 }
