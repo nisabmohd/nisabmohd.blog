32349932b98dcac71c1ebd769468e65494b8d090
nisabmohd <nisabmohd@gmail.com>
2023-11-14 22:28:21 +0530

    icon size fixed

--- src/components/md/note.tsx
+++ src/components/md/note.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from "react";
 export default function Note({ children }: PropsWithChildren) {
   return (
     <div className="border-[1px] rounded-md px-3 py-4 h-fit prose-p:m-0 my-6 flex flex-row items-start gap-3">
-      <InfoIcon className="w-[20px] h-[20px]" />
+      <InfoIcon className="min-w-[20px] min-h-[20px]" />
       <span className="text-sm text-muted-foreground">{children}</span>
     </div>
   );
