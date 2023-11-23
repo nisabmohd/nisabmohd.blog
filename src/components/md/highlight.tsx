4bbb78068252645647ac483b46bdfd907bef6526
Mohd. Nisab <nisabmohd@gmail.com>
2023-11-23 11:19:33 +0530

    Update highlight.tsx

--- src/components/md/highlight.tsx
+++ src/components/md/highlight.tsx
@@ -1,6 +1,6 @@
 export default function Highlight({ children }: { children: string }) {
   return (
-    <span className="dark:bg-zinc-800 dark:text-zinc-200 bg-zinc-200 text-zinc-700 px-2 rounded-md text-sm py-1 whitespace-nowrap">
+    <span className="dark:bg-zinc-900 dark:text-zinc-200 bg-zinc-200 text-zinc-700 px-2 rounded-md text-sm py-1 whitespace-nowrap">
       {children}
     </span>
   );
