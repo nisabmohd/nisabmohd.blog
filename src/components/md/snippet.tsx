03aa9673d8f96d60612a9bff146714cd1e01fa0e
nisabmohd <nisabmohd@gmail.com>
2023-11-28 22:42:06 +0530

    some changes for snippet compoennt

--- src/components/md/snippet.tsx
+++ src/components/md/snippet.tsx
@@ -1,6 +1,6 @@
 export default function Snippet({ children }: { children: string }) {
   return (
-    <code className="dark:bg-zinc-900 dark:text-zinc-400 bg-zinc-200 text-zinc-700 px-2 rounded-md text-sm py-1 whitespace-nowrap font-medium font-sans before:hidden after:hidden">
+    <code className="dark:bg-zinc-900 dark:text-zinc-400 bg-zinc-200 text-zinc-700 px-2 rounded-md text-sm py-1 whitespace-nowrap font-medium before:hidden after:hidden">
       {children}
     </code>
   );
