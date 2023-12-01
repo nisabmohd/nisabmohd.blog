5722ae13f691f8f2ea9b4f8b2cc1df398a3d5a58
nisabmohd <nisabmohd@gmail.com>
2023-12-01 23:01:13 +0530

    added iterables blog

--- src/components/md/tag.tsx
+++ src/components/md/tag.tsx
@@ -1,6 +1,6 @@
 export default function HTMLTag({ children }: { children: string }) {
   return (
-    <span className="dark:bg-zinc-800 bg-zinc-200 px-2 rounded-md text-sm py-1 text-sky-400 mx-[2px] font-semibold">
+    <span className="dark:bg-zinc-800 bg-zinc-200 px-2 rounded-md text-sm py-1 text-sky-500 mx-[2px] font-normal">
       {"<"}
       {children}
       {">"}
