5d81b3fe84616e38fadbf604b0d3104a0b846399
nisabmohd <nisabmohd@gmail.com>
2024-01-11 14:01:59 +0530

    not found page added

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h3 className="mb-10 text-[21px] font-medium"> 😥 Page not found</h3>
+      <Link className={buttonVariants({})} href="/">
+        Back to homepage
+      </Link>
+    </div>
+  );
+}
