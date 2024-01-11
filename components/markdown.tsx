4066fb6ac4e4b96d8023f10a41780e83ba3ae173
Mohd. Nisab <nisabmohd@gmail.com>
2024-01-11 13:53:30 +0530

    Update markdown.tsx

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -19,8 +19,8 @@ function NoWrap({ children }: PropsWithChildren) {
 
 function Note({ children }: PropsWithChildren) {
   return (
-    <div className=" dark:bg-zinc-900 bg-zinc-50 rounded-md px-3 py-4 h-fit prose-p:m-0 my-6 flex flex-row items-start gap-3 ">
-      <InfoIcon className="min-w-[18px] min-h-[18px] max-w-[19px] max-h-[19px] mt-3" />
+    <div className=" dark:bg-zinc-900 bg-zinc-50 rounded-md px-3 py-4 h-fit prose-p:m-0 my-6 flex flex-row items-center gap-3 ">
+      <InfoIcon className="min-w-[18px] min-h-[18px] max-w-[19px] max-h-[19px]" />
       <span className="text-[14px]">{children}</span>
     </div>
   );
