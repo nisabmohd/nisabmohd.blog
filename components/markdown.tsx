1a4f747231bd7aaa245300f7c042916b83a26e67
nisabmohd <nisabmohd@gmail.com>
2024-02-18 01:00:09 +0530

    ThemedImage component added

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -3,6 +3,7 @@ import { InfoIcon } from "lucide-react";
 import Image from "next/image";
 import { ComponentProps } from "react";
 import Link from "next/link";
+import ThemedImage from "./themed-img";
 import { CachedTweet } from "./tweet";
 
 function Highlight({ children }: PropsWithChildren) {
@@ -21,7 +22,7 @@ function Note({ children }: PropsWithChildren) {
   return (
     <div className="dark:bg-zinc-900 bg-zinc-50 rounded-md px-3 py-4 h-fit prose-p:m-0 my-6 flex flex-row items-center gap-3 ">
       <InfoIcon className="min-w-[18px] min-h-[18px] max-w-[19px] max-h-[19px]" />
-      <p className="text-sm">{children}</p>
+      <span className="text-sm">{children}</span>
     </div>
   );
 }
@@ -81,4 +82,5 @@ export const components = {
   Link,
   Quote,
   Tweet,
+  ThemedImage,
 };
---
+++ components/themed-img.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function ThemedImage({
+  filename,
+  ext,
+  height,
+  width,
+}: {
+  filename: string;
+  ext: string;
+  width?: number;
+  height?: number;
+}) {
+  const { resolvedTheme } = useTheme();
+  const [imagePath, setImagePath] = useState<string>();
+
+  useEffect(() => {
+    if (!resolvedTheme) return;
+    const exactFileName = "/" + filename + "-" + resolvedTheme + "." + ext;
+    setImagePath(exactFileName);
+  }, [resolvedTheme, filename, ext]);
+
+  if (!imagePath) return null;
+  return (
+    <Image
+      src={imagePath}
+      alt="img"
+      width={width ?? 400}
+      height={height ?? 400}
+      className="mx-auto"
+    />
+  );
+}
