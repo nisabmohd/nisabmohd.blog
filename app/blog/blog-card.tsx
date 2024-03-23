1a525822b1e60c4d35eecf38aa36f620a9a021a5
nisabmohd <nisabmohd@gmail.com>
2024-03-24 00:31:15 +0530

    server actions blog added

--- app/blog/blog-card.tsx
+++ app/blog/blog-card.tsx
@@ -5,7 +5,7 @@ type BlogCardProps = MDXFrontmatter;
 export default async function Blog({ slug, title, published }: BlogCardProps) {
   return (
     <Link href={`/blog/${slug}`}>
-      <h3 className="mb-1">{title}</h3>
+      <h3 className="sm:mb-1 mb-0.5">{title}</h3>
       <span className="text-[15px] text-muted-foreground">
         {new Date(published).toDateString()}
       </span>
