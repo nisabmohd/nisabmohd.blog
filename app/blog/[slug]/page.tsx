41fa55499d6f9f5a995edb527a64faeb50f64ac9
nisabmohd <nisabmohd@gmail.com>
2024-04-20 01:39:56 +0530

    some color enhancements

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -39,7 +39,7 @@ export default async function SpecificBlogPage({
   return (
     <div>
       <FrontMatter {...frontmatter} />
-      <div className="prose dark:prose-invert prose-neutral py-8 dark:prose-code:text-zinc-200 prose-code:text-[#354150] dark:prose-code:bg-zinc-900/30 dark:prose-pre:bg-zinc-900/30 prose-code:bg-stone-50 prose-pre:bg-stone-50 prose-pre:font-code prose-headings:font-medium underline-offset-2">
+      <div className="prose dark:prose-invert prose-neutral py-8 dark:prose-code:text-zinc-200 prose-code:text-[#354150] dark:prose-code:bg-neutral-900 dark:prose-pre:bg-neutral-900 prose-code:bg-zinc-50 prose-pre:bg-zinc-50 prose-pre:font-code prose-headings:font-medium underline-offset-2">
         {content}
       </div>
     </div>
