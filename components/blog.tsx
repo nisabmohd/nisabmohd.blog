c1a40081f1486170424d0a7876e757546fa5c1e8
Mohd. Nisab <nisabmohd@gmail.com>
2024-06-15 21:59:20 +0530

    some minor enhancements

--- components/blog.tsx
+++ components/blog.tsx
@@ -5,9 +5,9 @@ type BlogCardProps = MDXFrontmatter;
 
 export default function Blog({ title, slug, published }: BlogCardProps) {
   return (
-    <Link className="flex sm:flex-col flex-col gap-0.5" href={`/blog/${slug}`}>
+    <Link className="flex sm:flex-col flex-col gap-1" href={`/blog/${slug}`}>
       <p>{title}</p>
-      <span className="font-sans text-muted-foreground text-[15px]">
+      <span className="font-sans text-muted-foreground text-[14.5px]">
         {formatDate(new Date(published))}
       </span>
     </Link>
--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="mt-14 mb-8 dark:text-neutral-300 text-neutral-700 flex flex-col gap-7">
-      <div className="flex items-center gap-5">
+      <div className="flex items-center gap-4">
         <Link
           className="flex items-center gap-1.5"
           href="https://github.com/nisabmohd"
@@ -21,7 +21,7 @@ export default function Footer() {
           className="flex items-center gap-1.5"
           href="https://www.linkedin.com/in/mohd-nisab-725148197/"
         >
-          <MoveUpRightIcon className="w-4 h-4" /> Linkedin
+          <MoveUpRightIcon className="w-4 h-4" /> LinkedIn
         </Link>
       </div>
       <p className="text-[15px]">© 2024 MIT Licensed</p>
