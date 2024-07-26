23fdba66ac18ffbf6ebec1f943c4b0adbfd6832c
Mohd. Nisab <nisabmohd@gmail.com>
2024-07-27 00:10:22 +0530

    added new blog

--- components/note.tsx
+++ components/note.tsx
@@ -1,10 +1,13 @@
-import { OctagonAlertIcon } from "lucide-react";
+import { AlertCircleIcon } from "lucide-react";
 import { PropsWithChildren } from "react";
 
 export function Note({ children }: PropsWithChildren) {
   return (
-    <div className="dark:bg-neutral-900 bg-stone-50 rounded-md px-3 py-4 h-fit prose-p:m-0 my-6 flex flex-row items-center gap-4 border">
-      <OctagonAlertIcon className="min-w-[18px] min-h-[18px] max-w-[20px] max-h-[20px]" />
+    <div className="dark:bg-neutral-900 bg-stone-50 rounded-md px-3 py-4 h-fit prose-p:m-0 my-6 flex flex-col items-start gap-2 border">
+      <div className="flex items-center gap-2">
+        <AlertCircleIcon className="min-w-[18px] min-h-[18px] max-w-[18px] max-h-[18px]" />
+        <p className="text-sm font-semibold">Note</p>
+      </div>
       <span className="text-[14.5px]">{children}</span>
     </div>
   );
