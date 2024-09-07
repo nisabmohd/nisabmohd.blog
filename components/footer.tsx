7b738ad570ff2c02cb70106910684e8ca78a12b2
Mohd. Nisab <nisabmohd@gmail.com>
2024-09-07 21:01:38 +0530

    small ui change for consistent ui

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="h-20 flex items-center justify-between text-[14.5px] text-neutral-800 dark:text-neutral-100">
+    <footer className="h-20 flex items-center justify-between sm:text-[14.5px] text-[14px] text-neutral-800 dark:text-neutral-100">
       <p>Nisab Mohd © 2024</p>
       <div className="flex items-center gap-2">
-        <Link href="https://github.com/nisabmohd">GitHub</Link>
-        <Link href="https://twitter.com/MohdNisab">Twitter</Link>
+        <Link href="https://github.com/nisabmohd">github</Link>
+        <Link href="https://twitter.com/MohdNisab">twitter</Link>
         <Link href="https://www.linkedin.com/in/mohd-nisab-725148197/">
-          LinkedIn
+          linkedin
         </Link>
       </div>
     </footer>
