cdb4c0b387e3fde7db56cbed6e4eb46bcd8f54d5
Mohd. Nisab <nisabmohd@gmail.com>
2024-09-07 18:00:23 +0530

    added new blog

--- components/note.tsx
+++ components/note.tsx
@@ -2,10 +2,8 @@ import { PropsWithChildren } from "react";
 
 export function Note({ children }: PropsWithChildren) {
   return (
-    <div className="dark:bg-neutral-900 bg-neutral-100 rounded-md px-3 py-4 h-fit prose-p:m-0 my-6 flex flex-col items-start gap-2">
-      <div className="flex items-center gap-2">
-        <p className="text-sm font-semibold">Note</p>
-      </div>
+    <div className="dark:bg-neutral-900 bg-neutral-100 rounded-md px-3 py-4 h-fit prose-p:m-0 my-6 flex flex-col items-start gap-2 border dark:border-neutral-800">
+      <p className="text-sm font-semibold">Note</p>
       <span className="text-[14.5px]">{children}</span>
     </div>
   );
