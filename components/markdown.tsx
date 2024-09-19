2514114ef0ff14a011d000c9239ead772c03e84f
Mohd. Nisab <nisabmohd@gmail.com>
2024-09-20 01:12:40 +0530

    added new blog

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,18 +1,42 @@
 import { ComponentProps } from "react";
-import Image from "next/image";
-import Link from "next/link";
+import NextImage from "next/image";
+import NextLink from "next/link";
 import { Tweet } from "./tweet";
 import { Note } from "./note";
 import AdaptiveImage from "./adaptive-image";
 
-function Img({ ...rest }: ComponentProps<typeof Image>) {
-  return <Image {...rest} alt="img" />;
+type Height = ComponentProps<typeof NextImage>["height"];
+type Width = ComponentProps<typeof NextImage>["width"];
+
+function Image({
+  src,
+  alt = "alt",
+  width = 650,
+  height = 250,
+  ...props
+}: ComponentProps<"img">) {
+  if (!src) return null;
+  return (
+    <NextImage
+      src={src}
+      alt={alt}
+      width={width as Width}
+      height={height as Height}
+      quality={40}
+      {...props}
+    />
+  );
+}
+
+function Link({ href, ...props }: ComponentProps<"a">) {
+  if (!href) return null;
+  return <NextLink href={href} {...props} />;
 }
 
 export const components = {
   Note,
-  Link,
+  a: Link,
   Tweet,
-  Img,
+  img: Image,
   AdaptiveImage,
 };
