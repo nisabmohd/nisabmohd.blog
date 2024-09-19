b3739286883e1dc8d9df6854a1df3bc8fa110fec
Mohd. Nisab <nisabmohd@gmail.com>
2024-09-20 01:15:00 +0530

    moved markdown components to markdown file

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,9 +1,7 @@
-import { ComponentProps } from "react";
+import { ComponentProps, PropsWithChildren } from "react";
 import NextImage from "next/image";
 import NextLink from "next/link";
 import { Tweet } from "./tweet";
-import { Note } from "./note";
-import AdaptiveImage from "./adaptive-image";
 
 type Height = ComponentProps<typeof NextImage>["height"];
 type Width = ComponentProps<typeof NextImage>["width"];
@@ -31,6 +29,51 @@ function Image({
 function Link({ href, ...props }: ComponentProps<"a">) {
   if (!href) return null;
   return <NextLink href={href} {...props} />;
+}
+
+type AdaptiveImageProps = Exclude<ComponentProps<typeof NextImage>, "src"> & {
+  src: {
+    light: string;
+    dark: string;
+  };
+};
+
+function AdaptiveImage({
+  src: { dark, light },
+  alt = "themed-img",
+  width = 600,
+  height = 400,
+  ...rest
+}: AdaptiveImageProps) {
+  return (
+    <>
+      <NextImage
+        className="dark:hidden block"
+        src={light}
+        alt={alt}
+        width={width}
+        height={height}
+        {...rest}
+      />
+      <NextImage
+        className="dark:block hidden"
+        src={dark}
+        alt={alt}
+        width={width}
+        height={height}
+        {...rest}
+      />
+    </>
+  );
+}
+
+export function Note({ children }: PropsWithChildren) {
+  return (
+    <div className="dark:bg-neutral-900 bg-neutral-100 rounded-md px-3 py-4 h-fit prose-p:m-0 my-6 flex flex-col items-start gap-2 border dark:border-neutral-800">
+      <p className="text-sm font-semibold">Note</p>
+      <span className="text-[14.5px]">{children}</span>
+    </div>
+  );
 }
 
 export const components = {
